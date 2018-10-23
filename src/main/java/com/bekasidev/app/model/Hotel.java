78bc2bf7543578c8ac8de2511cd1d6395e662977
Unknown <sra.wahyudi@gmail.com>
2018-10-23 15:31:17 +0700

    Auto stash before merge of "master" and "origin/master"
    Model Hotel Transaction

---
+++ src/main/java/com/bekasidev/app/model/Hotel.java
@@ -0,0 +1,18 @@
+package com.bekasidev.app.model;
+
+/**
+ * Created by waddi on 23/10/18.
+ */
+public class Hotel {
+
+    private String idHotel;
+    private String namaHotel;
+
+    public String getIdHotel() {return idHotel; }
+
+    public void setIdHotel(String idHotel) {this.idHotel = idHotel; }
+
+    public String getNamaHotel() {return  namaHotel; }
+
+    public void setNamaHotel(String namaHotel) {this.namaHotel = namaHotel; }
+}
