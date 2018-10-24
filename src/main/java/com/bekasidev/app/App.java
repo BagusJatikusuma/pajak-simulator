3e785610df8d389f00a0b00e0c5b8c9b7b135eb4
Unknown <rony.natak@gmail.com>
2018-10-24 09:56:04 +0700

    create resto

--- src/main/java/com/bekasidev/app/App.java
+++ src/main/java/com/bekasidev/app/App.java
@@ -1,7 +1,10 @@
 package com.bekasidev.app;
 
+import com.bekasidev.app.model.Restoran;
 import com.bekasidev.app.model.RestoranTransaction;
+import com.bekasidev.app.service.backend.RestoranService;
 import com.bekasidev.app.service.backend.RestoranTransactionService;
+import com.bekasidev.app.service.backend.impl.RestoranServiceImpl;
 import com.bekasidev.app.service.backend.impl.RestoranTransactionServiceImpl;
 import com.bekasidev.app.view.FrameAttributeConstant;
 import com.bekasidev.app.view.MainFrame;
@@ -17,17 +20,22 @@ public class App
         /**
          * init main frame
          */
-        RestoranTransaction rt = new RestoranTransaction("123123",
-                "321321",
-                3000000,
-                1500000,
-                900000,
-                110,
-                150,
-                100,
-                360,645000000);
-        RestoranTransactionService restoranTransactionService = new RestoranTransactionServiceImpl();
-        System.out.println((int)restoranTransactionService.calculatePotensiPajakRestoran(rt));
+//        RestoranTransaction rt = new RestoranTransaction("123123",
+//                "321321",
+//                3000000,
+//                1500000,
+//                900000,
+//                110,
+//                150,
+//                100,
+//                0,0);
+//        Restoran restoran = new Restoran();
+//        restoran.setNamaRestoran("SOLARIA");
+//        RestoranTransactionService restoranTransactionService = new RestoranTransactionServiceImpl();
+//        RestoranService restoranService = new RestoranServiceImpl();
+//        restoranService.createDataRestoran(restoran);
+//        System.out.println((int)restoranTransactionService.calculatePotensiPajakRestoran(rt));
+//        restoranTransactionService.createRestoranTransaction(rt);
         MainFrame mainFrame = new MainFrame();
         mainFrame.init(FrameAttributeConstant.MAIN_FRAME_WIDTH, 
                         FrameAttributeConstant.MAIN_FRAME_HEIGHT, 
