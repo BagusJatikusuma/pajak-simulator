96903a39a7cc3095540d69fa7f464d135d1550f5
Bagus Jatikusuma <jatikusuma0801@gmail.com>
2018-10-25 13:22:39 +0700

    add table component

---
+++ src/main/java/com/bekasidev/app/view/HotelTableComponent.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bekasidev.app.view;
+
+import com.bekasidev.app.model.Hotel;
+import com.bekasidev.app.model.Restoran;
+import com.bekasidev.app.service.backend.HotelService;
+import com.bekasidev.app.service.backend.impl.HotelServiceImpl;
+import com.bekasidev.app.service.backend.impl.RestoranServiceImpl;
+import java.util.List;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author bagus
+ */
+public class HotelTableComponent extends JPanel {
+    private HotelService service;
+    
+    public void init() {
+        service = new HotelServiceImpl();
+        
+        JScrollPane jScrollPane1 = new JScrollPane();
+        JTable restoranTable = new JTable();
+        DefaultTableModel dtm = new DefaultTableModel(0,0);
+        String header[] = new String[] {"ID_HOTEL", "NAMA_HOTEL"};
+        dtm.setColumnIdentifiers(header);
+        restoranTable.setModel(dtm);
+        
+        List<Hotel> hotels = service.getAllHotel();
+        
+        for (Hotel obj : hotels) {
+            dtm.addRow(new Object[] {obj.getIdHotel(), obj.getNamaHotel()});
+        }
+        
+        restoranTable.setGridColor(new java.awt.Color(255, 255, 255));
+        restoranTable.setRowHeight(22);
+        jScrollPane1.setViewportView(restoranTable);
+        
+        jScrollPane1.setSize(400, 300);
+        jScrollPane1.setLocation(50, 330);
+        
+        this.add(jScrollPane1);
+    }
+    
+}
