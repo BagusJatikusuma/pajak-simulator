7c352ce949e26738ff61741873f36506aa5d7680
Bayu Arafli Fauzi <bayuaraflifauzi@gmail.com>
2018-10-25 08:03:55 +0700

    commit bayu

--- src/main/java/com/bekasidev/app/view/ContentPanel.java
+++ src/main/java/com/bekasidev/app/view/ContentPanel.java
@@ -8,7 +8,10 @@ package com.bekasidev.app.view;
 import com.bekasidev.app.model.Restoran;
 import com.bekasidev.app.service.backend.RestoranService;
 import com.bekasidev.app.service.backend.impl.RestoranServiceImpl;
+import static com.bekasidev.app.view.FrameAttributeConstant.MAIN_FRAME_WIDTH;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -36,351 +39,81 @@ import javax.swing.table.DefaultTableModel;
 public class ContentPanel extends JPanel{
 
     // Variables declaration
-    private JTextField tfNamaRestoran, tfPemilik, tfTotalUang;
-    private JLabel labelPilihanLevel, labelPilihanRestoran;
-    private JRadioButton level1, level2, level3;
-    private JCheckBox goceng, ceban, gocap;
-    
-    private String[] level, restoran;
-    private int[] uang;
-    private int total = 0;
     
     public ContentPanel() {
         super();
     }
     
     public void initPanel(){
-        //setting layout content panel
+        // setting layout content panel
         this.setLayout(null);
         this.setBackground(Color.WHITE);
         
-        //form
-        JLabel labelNamaRestoran = new JLabel("Nama Restoran");
-        labelNamaRestoran.setOpaque(true);
-        labelNamaRestoran.setBackground(Color.red);
-        labelNamaRestoran.setSize(150, 20);
-        labelNamaRestoran.setLocation(50, 50);
+        // restauran
+        restauranContent();
+                
+    }
+    
+    public void restauranContent(){
+        //===== header =====//
+        Dimension screenSize = this.getToolkit().getScreenSize();
+        JPanel panelHeader = new JPanel();
+        panelHeader.setLayout(null);
+        panelHeader.setBackground(Color.decode("#4377ca"));
+        panelHeader.setSize((int) (screenSize.getWidth()-250), 50);
+        panelHeader.setLocation(0, 0);
         
-        tfNamaRestoran = new JTextField();
-        tfNamaRestoran.setSize(250, 20);
-        tfNamaRestoran.setLocation(210, 50);
+        JLabel labelHeader = new JLabel("FORM IDENTITAS");
+        labelHeader.setFont(new Font("Tahoma", Font.BOLD, 20));
+        labelHeader.setForeground(Color.WHITE);
+        labelHeader.setSize(200, 50);
+        labelHeader.setLocation(25, 0);
         
-        JLabel labelPemilik = new JLabel("Pemilik");
-        labelPemilik.setOpaque(true);
-        labelPemilik.setBackground(Color.red);
-        labelPemilik.setSize(150, 20);
-        labelPemilik.setLocation(50, 80);
+        RestoranService restoranService = new RestoranServiceImpl();
         
-        tfPemilik = new JTextField();
-        tfPemilik.setSize(250, 20);
-        tfPemilik.setLocation(210, 80);
-
-        JButton submit = new JButton("Submit");
-        submit.setSize(100, 20);
-        submit.setLocation(210, 110);
-        submit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {             
-                System.out.println("Nama Restoran : " + tfNamaRestoran.getText() + "\nPemilik : " + tfPemilik.getText());
-            }
-        });
+        // add to panelHeader
+        panelHeader.add(labelHeader);
+        //===== header =====//
         
-        // Add to panel
-        this.add(labelNamaRestoran);
-        this.add(tfNamaRestoran);
-        this.add(labelPemilik);
-        this.add(tfPemilik);
-        this.add(submit);
+        //===== combo box =====//
+        List<Restoran> listRestoran = new ArrayList<>();
+        listRestoran = restoranService.getAllRestoran();
+        Restoran restorans[];
+        restorans = new Restoran[listRestoran.size()];
+        for (int i = 0; i < listRestoran.size(); i++) {
+            restorans[i] = listRestoran.get(i);
+        }
         
-        showRestaurantTable();
+        JComboBox<Restoran> cbListRestoran = new JComboBox<Restoran>(restorans);
+        cbListRestoran.setForeground(Color.BLUE);
+        cbListRestoran.setFont(new Font("Tahoma", Font.BOLD, 16));
+        cbListRestoran.setMaximumRowCount(5);
+        cbListRestoran.setSize(200,20);
+        cbListRestoran.setLocation(210,70);
         
-        
-        // Contoh menggunakan combo box
-        JLabel labelListRestoran = new JLabel("Nama Restoran");
-        labelListRestoran.setOpaque(true);
-        labelListRestoran.setBackground(Color.red);
-        labelListRestoran.setSize(150, 20);
-        labelListRestoran.setLocation(50, 150);
-        
-        restoran = new String[] {"Rumah Makan Cap Manjur", "Rumah Makan SK", "Masak Suka Makan Suka", "Warung Makan Minum"};
-        JComboBox<String> restoranList = new JComboBox<>(restoran);
-        
-        // style
-//        restoranList.setForeground(Color.BLUE);
-//        restoranList.setFont(new Font("Arial", Font.BOLD, 14));
-//        restoranList.setMaximumRowCount(5);
-        
-        // potition and size
-        restoranList.setSize(250, 20);
-        restoranList.setLocation(210, 150);
-        
-        labelPilihanRestoran = new JLabel();
-        labelPilihanRestoran.setOpaque(true);
-        labelPilihanRestoran.setBackground(Color.red);
-        labelPilihanRestoran.setSize(150, 20);
-        labelPilihanRestoran.setLocation(470, 150);
-        
-        labelPilihanRestoran.setText(restoran[0]);
-        
-        // get the selected item:
-        restoranList.addActionListener(new ActionListener() {
+        cbListRestoran.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
-                JComboBox<String> combo = (JComboBox<String>) event.getSource();
-                String selectedBook = (String) combo.getSelectedItem();
-
-                if (selectedBook.equals(restoran[0])) {
-                    labelPilihanRestoran.setText(restoran[0]);
-                } else if (selectedBook.equals(restoran[1])) {
-                    labelPilihanRestoran.setText(restoran[1]);
-                } else if (selectedBook.equals(restoran[2])) {
-                    labelPilihanRestoran.setText(restoran[2]);
-                }else if (selectedBook.equals(restoran[3])) {
-                    labelPilihanRestoran.setText(restoran[3]);
-                }
+                JComboBox<Restoran> combo = (JComboBox<Restoran>) event.getSource();
+                Restoran selected = (Restoran) combo.getSelectedItem();
+                System.out.println("Nama : " + selected.getNamaRestoran() + "\nId : " + selected.getIdRestoran());
             }
         });
+        //===== combo box =====//
         
 
-        // add to panel
-        this.add(labelListRestoran);
-        this.add(labelPilihanRestoran);
-        this.add(restoranList);
+        //===== form restoran =====//
+        JLabel labelNamaRestoran = new JLabel("Nama Restoran");
+        labelNamaRestoran.setFont(new Font("Tahoma", 1, 16));
+        labelNamaRestoran.setForeground(Color.BLACK);
+        labelNamaRestoran.setSize(150, 50);
+        labelNamaRestoran.setLocation(50, 70);
         
-        
-        //contoh menggunakan radio button
-        JLabel labelLevel = new JLabel("Pilih Level");
-        labelLevel.setOpaque(true);
-        labelLevel.setBackground(Color.red);
-        labelLevel.setSize(150, 20);
-        labelLevel.setLocation(50, 190);
-        
-        level = new String[] {"Level 1", "Level 2", "Level 3"};
-        level1 = new JRadioButton(level[0]);
-        level2 = new JRadioButton(level[1]);
-        level3 = new JRadioButton(level[2]);
-        
-        ButtonGroup group = new ButtonGroup();
-        group.add(level1);
-        group.add(level2);
-        group.add(level3);
-        
-        // layout
-        JPanel panelRadioButton = new JPanel(new GridBagLayout());
-        
-        // specify constraints
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 0;
-        panelRadioButton.add(level1, constraints);
-        constraints.gridx = 1;
-        panelRadioButton.add(level2, constraints);
-        constraints.gridx = 2;
-        panelRadioButton.add(level3, constraints);
-        
-        panelRadioButton.setSize(250, 20);
-        panelRadioButton.setLocation(210, 190);
-        
-        labelPilihanLevel = new JLabel();
-        labelPilihanLevel.setOpaque(true);
-        labelPilihanLevel.setBackground(Color.red);
-        labelPilihanLevel.setSize(140, 20);
-        labelPilihanLevel.setLocation(320, 220);
-        
-        //button radio button
-        JButton submitRB = new JButton("Cek");
-        submitRB.setSize(100, 20);
-        submitRB.setLocation(210, 220);
-        submitRB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (level1.isSelected()) {
-                    labelPilihanLevel.setText(level[0]);
-                } else if (level2.isSelected()) {
-                    labelPilihanLevel.setText(level[1]);
-                } else if (level3.isSelected()) {
-                    labelPilihanLevel.setText(level[2]);
-                } else {
-                    labelPilihanLevel.setText("pilih terlebih dahulu");
-                }
-            }
-        });
         
         // add to panel
-        this.add(labelLevel);
-        this.add(panelRadioButton);
-        this.add(submitRB);
-        this.add(labelPilihanLevel);
-        
-        
-        // Contoh menggunakan cek box
-        JLabel labelUang = new JLabel("Uang");
-        labelUang.setOpaque(true);
-        labelUang.setBackground(Color.red);
-        labelUang.setSize(150, 20);
-        labelUang.setLocation(50, 250);
-        
-        uang = new int[] {5000, 10000, 50000};
-        
-        goceng = new JCheckBox(String.valueOf(uang[0]));
-        ceban = new JCheckBox(String.valueOf(uang[1]));
-        gocap = new JCheckBox(String.valueOf(uang[2]));
-        
-        // layout
-        JPanel panelCheckBox = new JPanel(new GridBagLayout());
-        
-        // specify constraints
-        GridBagConstraints constraintsBox = new GridBagConstraints();
-        constraintsBox.gridx = 0;
-        panelCheckBox.add(goceng, constraintsBox);
-        constraintsBox.gridx = 1;
-        panelCheckBox.add(ceban, constraintsBox);
-        constraintsBox.gridx = 2;
-        panelCheckBox.add(gocap, constraintsBox);
-        
-        panelCheckBox.setSize(250, 20);
-        panelCheckBox.setLocation(210, 250);
-        
-        JLabel labelTotalUang = new JLabel("Total Rp.");
-        labelTotalUang.setOpaque(true);
-        labelTotalUang.setBackground(Color.red);
-        labelTotalUang.setSize(100, 20);
-        labelTotalUang.setLocation(210, 280);
-        
-        tfTotalUang = new JTextField();
-        tfTotalUang.setSize(140, 20);
-        tfTotalUang.setLocation(320, 280);
-        
-        // add to panel
-        this.add(labelUang);
-        this.add(labelTotalUang);
-        this.add(tfTotalUang);
-        this.add(panelCheckBox);
-        
-        // add action listener for the check boxes
-        ActionListener actionListener = new ActionHandler(); 
-        goceng.addActionListener(actionListener);
-        ceban.addActionListener(actionListener);
-        gocap.addActionListener(actionListener);
-        
+        this.add(cbListRestoran);
+        this.add(panelHeader);
+        this.add(labelNamaRestoran);
     }
-    
-    public void addTable() {
-        JScrollPane jScrollPane1 = new JScrollPane();
-        JTable jTable1 = new JTable();
-        
-//        jScrollPane1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
-        
-        jTable1.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "ROM Gen", "US", null},
-                {"12/1/2018", "Text Ed", "UK", null},
-                {"12/1/2018", "Mola Con", "China", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null},
-                {"12/1/2018", "Expresso POS", "Kenya", null}
-            },
-            new String [] {
-                "Date", "Item", "Location", "Completed"
-            }
-        ) {
-            Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Boolean.class
-            };
-
-            @Override
-            public Class getColumnClass(int columnIndex) {
-                return types[columnIndex];
-            }
-        });
-        jTable1.setGridColor(new java.awt.Color(255, 255, 255));
-        jTable1.setRowHeight(22);
-        jScrollPane1.setViewportView(jTable1);
-        
-        jScrollPane1.setSize(400, 300);
-        jScrollPane1.setLocation(200, 200);
-        
-        this.add(jScrollPane1);
-    }
-    
-    public void showRestaurantTable() {
-        JScrollPane jScrollPane1 = new JScrollPane();
-        JTable jTable1 = new JTable();
-        
-        DefaultTableModel dtm = new DefaultTableModel(0, 0);
-        String header[] = new String[] {"ID_RESTAURANT", "NAMA_RESTAURANT"};
-        dtm.setColumnIdentifiers(header);
-        jTable1.setModel(dtm);
-        
-        RestoranService service = new RestoranServiceImpl();
-        
-        List<Restoran> restorans = service.getAllRestoran();
-//        for (int i=0; i<20; i++) {
-//            Restoran obj = new Restoran();
-//            obj.setIdRestoran("id "+1);
-//            obj.setNamaRestoran("Restoran "+i);
-//            
-//            restorans.add(obj);
-//        }
-
-        for (Restoran obj : restorans) {
-            dtm.addRow(new Object[] {obj.getIdRestoran(), obj.getNamaRestoran()});
-        }
-        
-        jTable1.setGridColor(new java.awt.Color(255, 255, 255));
-        jTable1.setRowHeight(22);
-        jScrollPane1.setViewportView(jTable1);
-        
-        jScrollPane1.setSize(400, 300);
-        jScrollPane1.setLocation(50, 330);
-        
-        this.add(jScrollPane1);
-    }
-    
-    
-    class ActionHandler implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent event) {
-            JCheckBox checkbox = (JCheckBox) event.getSource();
-            if (checkbox.isSelected()) {
-                if (checkbox == goceng) {
-                        total += uang[0];
-                } else if (checkbox == ceban) {
-                        total += uang[1];
-                } else if (checkbox == gocap) {
-                        total += uang[2];
-                }
-            } else {
-                if (checkbox == goceng) {
-                        total -= uang[0];
-                } else if (checkbox == ceban) {
-                        total -= uang[1];
-                } else if (checkbox == gocap) {
-                        total -= uang[2];
-                }
-            }
-                tfTotalUang.setText(String.valueOf(total));
-        }
-    }
-    
+           
 }
