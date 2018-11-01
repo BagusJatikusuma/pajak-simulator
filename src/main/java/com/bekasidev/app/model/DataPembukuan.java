267ce967096bc0ab3931d8616215716ded2b1354
Unknown <sra.wahyudi@gmail.com>
2018-11-01 14:00:46 +0700

    Auto stash before merge of "master" and "origin/master"
    update pembaharuan

--- src/main/java/com/bekasidev/app/model/DataPembukuan.java
+++ src/main/java/com/bekasidev/app/model/DataPembukuan.java
@@ -7,11 +7,23 @@ public class DataPembukuan {
 
     private String idHotel;
     private String idDataPembukuan;
+    private String labelBarang;
     private String uraianDataPembukuan;
     private Integer jumlahDataPembukuan;
-    private Integer jumlahTotalDataPembukuan;
     private String satuanBarang;
     private String tanggalBuat;
+
+    public DataPembukuan(){}
+
+    public DataPembukuan(String idHotel, String idDataPembukuan, String labelBarang,String uraianDataPembukuan, Integer jumlahDataPembukuan, String satuanBarang, String tanggalBuat) {
+        this.idHotel = idHotel;
+        this.idDataPembukuan = idDataPembukuan;
+        this.labelBarang = labelBarang;
+        this.uraianDataPembukuan = uraianDataPembukuan;
+        this.jumlahDataPembukuan = jumlahDataPembukuan;
+        this.satuanBarang = satuanBarang;
+        this.tanggalBuat =tanggalBuat;
+    }
 
     public String getIdHotel() { return idHotel;}
 
@@ -21,6 +33,10 @@ public class DataPembukuan {
 
     public void setIdDataPembukuan(String idDataPembukuan) {this.idDataPembukuan = idDataPembukuan; }
 
+    public String getLabelBarang() {return labelBarang; }
+
+    public void setLabelBarang(String labelBarang) {this.labelBarang = labelBarang; }
+
     public String getUraianDataPembukuan() { return uraianDataPembukuan; }
 
     public void setUraianDataPembukuan(String uraianDataPembukuan) {this.uraianDataPembukuan = uraianDataPembukuan; }
@@ -28,10 +44,6 @@ public class DataPembukuan {
     public Integer getJumlahDataPembukuan() { return jumlahDataPembukuan; }
 
     public void setJumlahDataPembukuan(Integer jumlahDataPembukuan) {this.jumlahDataPembukuan = jumlahDataPembukuan; }
-
-    public Integer getJumlahTotalDataPembukuan() {return jumlahTotalDataPembukuan;}
-
-    public void setJumlahTotalDataPembukuan(Integer jumlahTotalDataPembukuan) {this.jumlahTotalDataPembukuan = jumlahTotalDataPembukuan; }
 
     public String getSatuanBarang() { return satuanBarang; }
 
