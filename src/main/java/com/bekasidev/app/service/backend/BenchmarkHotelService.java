80d9d0a4a4848c8d030e2f64f51a70bd39252b97
Unknown <sra.wahyudi@gmail.com>
2018-11-08 09:21:59 +0700

    update

--- src/main/java/com/bekasidev/app/service/backend/BenchmarkHotelService.java
+++ src/main/java/com/bekasidev/app/service/backend/BenchmarkHotelService.java
@@ -1,8 +1,15 @@
 package com.bekasidev.app.service.backend;
 
+import com.bekasidev.app.model.BenchmarkHotel;
+
+import java.util.List;
+
 /**
  * Created by waddi on 05/11/18.
  */
 public interface BenchmarkHotelService {
-        
+
+    List<BenchmarkHotel> getAllBenchmark(String idHotel, String idBenchmarkHotel);
+
+    void createBenchmarkHotel(BenchmarkHotel benchmarkHotel);
 }
