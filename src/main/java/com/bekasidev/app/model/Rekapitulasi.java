bb15cc5ae7e7ebfaaf889374fdabccbbe74089b8
Unknown <rony.natak@gmail.com>
2018-11-27 09:15:15 +0700

    db

---
+++ src/main/java/com/bekasidev/app/model/Rekapitulasi.java
@@ -0,0 +1,15 @@
+package com.bekasidev.app.model;
+
+public class Rekapitulasi {
+
+    private String bulan;
+    private Double omzetHasilPeriksa;
+    private Double pajakHasilPeriksa;
+    private Double omzetLaporan;
+    private Double pajakDisetor;
+    private Double omzet;
+    private Double pokokPajak;
+    private Double denda;
+    private int persentaseDenda;
+    private Double jumlah;
+}
