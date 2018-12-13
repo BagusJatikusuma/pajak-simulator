038f2f1e2ce502c7f67d95e815a01ee1421bf3ba
Bagus Jatikusuma <jatikusuma0801@gmail.com>
2018-12-13 13:51:32 +0700

    integrasi login dengan mainframe

--- src/main/java/com/bekasidev/app/viewfx/javafxapplication/login/FormLoginController.java
+++ src/main/java/com/bekasidev/app/viewfx/javafxapplication/login/FormLoginController.java
@@ -8,18 +8,25 @@ package com.bekasidev.app.viewfx.javafxapplication.login;
 import com.bekasidev.app.service.ServiceFactory;
 import javafx.scene.image.Image;
 import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -69,11 +76,44 @@ public class FormLoginController implements Initializable {
             statusLogin = ServiceFactory.getUserLoginService().login(usernameField.getText(), passwordField.getText());
             if(statusLogin == true){
                 System.out.println("Berhasil masuk");
+                showMainApp();
             } else {
                 noticeField.setVisible(true);
                 noticeField.setText("Username dan Password yang Anda masukkan salah!");
                 noticeField.setAlignment(Pos.CENTER);
             }
         }
+    }
+    
+    private void showMainApp() {
+        closeLoginFrame();
+        
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        
+        Stage primaryStage = new Stage();
+        Image img = new Image(getClass().getResourceAsStream("/images/logo_kab_bekasi.png"));
+        primaryStage.getIcons().add(img);
+        
+        primaryStage.setTitle("Aplikasi Pemeriksaan Pajak");
+        primaryStage.setMaximized(true);
+        primaryStage.setWidth(primaryScreenBounds.getWidth());
+        primaryStage.setHeight(primaryScreenBounds.getHeight());
+        
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("javafxresources/RootPane.fxml"));
+        } catch (IOException ex) {
+            Logger.getLogger(FormLoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.resizableProperty().setValue(Boolean.FALSE);
+        primaryStage.show();
+        
+    }
+    
+    private void closeLoginFrame() {
+        Stage stage = (Stage) loginBtn.getScene().getWindow();
+        stage.close();
     }
 }
